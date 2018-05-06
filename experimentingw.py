83cdffe10591439962666e7d56a49e187941f6c4
Mina Sorsok <msorsok@student.unimelb.edu.au>
2018-05-06 15:02:36 +1000

    removed tdLeaf from git

---
+++ experimentingw.py
@@ -0,0 +1,122 @@
+import os
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+import numpy as np
+import pickle
+
+WHITE, BLACK, CORNER, BLANK, REMOVED = ['O','@','X','-',' ']
+ENEMIES = {WHITE: {BLACK, CORNER}, BLACK: {WHITE, CORNER}}
+FRIENDS = {WHITE: {WHITE, CORNER}, BLACK: {BLACK, CORNER}}
+BOARD_SIZE = 8
+
+#np.random.seed(42)
+
+class NeuralNet:
+
+    def __init__(self):
+        # hyperparameters
+        self.episode_number = 0
+        self.batch_size = 10
+        self.gamma = 0.99  # discount factor for reward
+        self.decay_rate = 0.99
+        self.hidden = 32
+        self.input = 64
+        self.learning_rate = 0.1
+        self.reward_sum = 0
+        self.myLambda = 0.7
+        self.running_reward = None
+        self.prev_processed_observations = None
+        self.weights = {}
+
+        try:
+            self.weights = pickle.load(open("weights.p-w", "rb"))
+
+        except:
+            self.weights["w1"] = np.random.random_sample((self.hidden, self.input))
+            print(self.weights["w1"])
+            self.weights["w2"] = np.random.random_sample(self.hidden)
+            self.weights["w2"] = self.weights["w2"] / np.amax(np.abs(self.weights["w2"]))
+            print(self.weights["w2"])
+
+        pickle.dump(self.weights, open("weights.p-w", "wb"))
+
+        self.batch_observations = []
+        self.batch_layer1 = []
+        self.batch_layer2 = []
+
+
+    def sigmoid(self, x):
+        return 1.0/(1.0 + np.exp(-x))
+
+    def relu(self, x):
+        if x < 0:
+            return 0
+        return x
+
+    def prepro(self, board, colour):
+        #returns 1D vector of board pieces
+        vector =[]
+        for x in range(BOARD_SIZE):
+            for y in range(BOARD_SIZE):
+                piece = board.grid[(x,y)]
+                if piece in ENEMIES[colour]:
+                    vector.append(-1)
+                elif piece == colour:
+                    vector.append(1)
+                else:
+                    vector.append(0)
+        return vector
+
+    def evaluateBoardAdvanced(self, board, colour):
+        self.weights = pickle.load(open("weights.p-w", "rb"))
+        observations = self.prepro(board, colour)
+        layer1 = np.dot(self.weights["w1"], observations)
+        layer1_activated = list(map(self.relu, layer1))
+        layer2 = np.dot(self.weights["w2"], layer1_activated)
+        return self.sigmoid(layer2), layer2, layer1
+
+    def deriv_sigmoid(self, x):
+        return np.exp(-x) / ((1.0 + np.exp(-x))**2)
+
+    def deriv_relu(self, x):
+        if x <= 0:
+            return 0
+        return 1
+
+
+
+    def updateWeight1(self, weight, index):
+        tdLeaf = pickle.load(open("tdLeaf.p", "rb"))
+        term1 = 0
+        for x in range(1, len(tdLeaf)):
+            term2 = 0
+            for y in range(1, len(tdLeaf)):
+                term2 += (self.myLambda ** (y - x)) * (tdLeaf[y][0] - tdLeaf[y - 1][0])
+            term1 += term2 * self.deriv_sigmoid(tdLeaf[x-1][1]) * self.weights["w2"][index] * self.deriv_relu(tdLeaf[x-1][2][index] * weight)
+        new_weight = weight + term1 * self.learning_rate
+        return new_weight
+
+
+    def updateWeight2(self,weight):
+        tdLeaf = pickle.load(open("tdLeaf.p", "rb"))
+        term1 = 0
+        for x in range(1, len(tdLeaf)):
+            term2 = 0
+            for y in range(1, len(tdLeaf)):
+                term2 += (self.myLambda ** (y - x)) * (tdLeaf[y][0] - tdLeaf[y - 1][0])
+            term1 += term2 * self.deriv_sigmoid(tdLeaf[x-1][1]) * weight
+        new_weight = weight + term1 * self.learning_rate
+        return new_weight
+
+    def updateWeights(self):
+        for index in range(len(self.weights["w2"])):
+            self.weights["w1"][index] = np.array([self.updateWeight1(x, index) for x in self.weights["w1"][index]])
+
+        self.weights["w2"] = np.array(list(map(self.updateWeight, self.weights["w2"])))
+        self.weights["w2"] = self.weights["w2"] / np.amax(np.abs(self.weights["w2"]))
+        pickle.dump(self.weights, open("weights.p-w", "wb"))
+
+        return
+
---
+++ player-w.py
@@ -0,0 +1,182 @@
+
+from state_representation import *
+from experimentingw import NeuralNet
+# initialise player
+# set up internal representation of the board
+# colour is either 'white' or 'black'
+
+import pickle
+
+BOARD_SIZE = 8
+PLACING_ACTIONS = 12
+SHRINK_ACTIONS = [75, 107]
+SHRINK_BEFORE = [128, 192]
+SHRINK_AFTER = [127, 191]
+SHRINK1 = [127, 128]
+SHRINK2 = [191, 192]
+
+class Player:
+
+    def __init__(self, colour):
+        self.actions = 0
+        self.board = self.initialiseBoard()
+        self.neuralNet = NeuralNet()
+
+        if colour.upper() == "BLACK":
+            self.colour = BLACK
+        else:
+            self.colour = WHITE
+
+
+    """actions a player can make -
+        make place a piece on a square, use tuple (x,y)
+        move a piece from one square to another, use nested tuple ((a,b), (c,d))
+        forfeit their turn, use value None """
+
+    # player selects next action and returns it given current state of the board
+    # turns - number of turns that have taken place since start of current game phase
+    def action(self, turns):
+        if turns in SHRINK_BEFORE:
+            self.shrink(turns)
+
+
+        if self.actions < PLACING_ACTIONS:
+            actions = self.generatePlacingActions()
+
+        else:
+            actions = self.generateMovingActions()
+
+        bestProb = 0
+        bestAction = None
+        bestScore = 0
+        bestHiddenScore = 0
+
+        if actions:
+            for action in actions:
+                curr_prob, score, hiddenScore = self.evaluateAction(action)
+                if curr_prob > bestProb:
+                    bestAction = action
+                    bestProb = curr_prob
+                    bestScore = score
+                    bestHiddenScore = hiddenScore
+
+            self.updateBoard(bestAction, self.colour)
+        self.actions += 1
+
+        if turns in SHRINK_AFTER:
+            self.shrink(turns)
+        return bestAction
+
+
+    # update player about opponents moves
+    def update(self, action):
+        if self.colour == BLACK:
+            enemyColour = WHITE
+        else:
+            enemyColour = BLACK
+        self.updateBoard(action, enemyColour)
+        return
+
+
+    #returns a new board object with no pieces on it
+    def initialiseBoard(self):
+        layout = [ [ BLANK for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+        for x,y in [(0,0),(0,BOARD_SIZE -1),(BOARD_SIZE - 1, 0), (BOARD_SIZE - 1, BOARD_SIZE - 1)]:
+            layout[x][y] = CORNER
+        return Board(layout)
+
+
+    #returns a list of all possible actions in placing phase
+    def generatePlacingActions(self):
+        if self.colour == WHITE:
+            y_start = 0
+            y_end = 6
+        else:
+            y_start = 2
+            y_end = BOARD_SIZE
+
+        actions = []
+        for x in range(BOARD_SIZE):
+            for y in range(y_start, y_end):
+                if self.board.grid[(x,y)] == BLANK:
+                    actions.append((x,y))
+        return actions
+
+    #returns a list of all possible actions in moving phase
+    def generateMovingActions(self):
+        actions = []
+        if self.colour == BLACK:
+            pieces = self.board.black_pieces
+        else:
+            pieces = self.board.white_pieces
+
+        for piece in pieces:
+            actions += piece.moves()
+        return actions
+
+    #evaluates the probability of winning if a certain action is taken
+    def evaluateAction(self, action):
+
+        x, y = action
+        #if placing action
+        if (isinstance(x, int) and isinstance(y, int)):
+            piece = Piece(self.colour, (x,y), self.board)
+            eliminated = piece.makemove((x,y))
+            prob, score, hiddenScore = self.neuralNet.evaluateBoardAdvanced(self.board, self.colour)
+            piece.undomove((x, y), eliminated)
+            piece.eliminate()
+
+        #if moving action
+        else:
+            piece = self.board.find_piece(x)
+            eliminated = piece.makemove(y)
+            prob, score, hiddenScore = self.neuralNet.evaluateBoardAdvanced(self.board, self.colour)
+            piece.undomove(x, eliminated)
+
+        return prob, score, hiddenScore
+
+    #enacts action on board
+    def updateBoard(self, action, colour):
+        if action:
+            x, y = action
+            if (isinstance(x, int) and isinstance(y, int)):
+                piece = Piece(colour, (x, y), self.board)
+                piece.makemove((x, y))
+            else:
+                piece = self.board.find_piece(x)
+                piece.makemove(y)
+            return
+
+
+    def shrink(self, turns):
+        if turns in SHRINK1:
+            edge = [0, BOARD_SIZE - 1]
+            corners = [(1, 1), (1, 6), (6, 6), (6, 1)]
+        elif turns in SHRINK2:
+            edge = [1, BOARD_SIZE - 2]
+            corners = [(2, 2), (2, 5), (5, 5), (5, 2)]
+        else:
+            print("shrinking gone wrong")
+
+        for x in range(BOARD_SIZE):
+            for y in range(BOARD_SIZE):
+                if x in edge or y in edge:
+                    piece = self.board.find_piece((x,y))
+                    if piece:
+                        piece.eliminate()
+                    self.board.grid[(x,y)] = REMOVED
+
+        for corner in corners:
+            piece = self.board.find_piece(corner)
+            if piece:
+                piece.eliminate()
+            self.board.grid[corner] = CORNER
+            for direction in DIRECTIONS:
+                adjacent_square = tuple(map(sum, zip(corner, direction)))
+                opposite_square = tuple(map(sum, zip(adjacent_square, direction)))
+                adjacent_piece = self.board.find_piece(adjacent_square)
+                opposite_piece = self.board.find_piece(opposite_square)
+                if adjacent_piece and opposite_piece:
+                    if adjacent_piece.player != opposite_piece.player:
+                        adjacent_piece.eliminate()
+        return
